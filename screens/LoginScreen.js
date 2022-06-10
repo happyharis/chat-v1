28a97825eaa0074fa1f28709bc068a44f0539638
Haris Samingan <muhamad_haris@hotmail.com>
2022-06-10 14:27:19 +0800

    Add error message

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,5 @@
 import {
+  Keyboard,
   StyleSheet,
   Text,
   TextInput,
@@ -14,8 +15,10 @@ const auth = firebase.auth();
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorText, setErrorText] = useState("");
 
   function login() {
+    Keyboard.dismiss();
     auth
       .signInWithEmailAndPassword(email, password)
       .then(() => {
@@ -23,38 +26,42 @@ export default function LoginScreen({ navigation }) {
       })
       .catch((error) => {
         console.log("Error");
+        setErrorText(error.message);
       });
   }
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Chat App</Text>
-        <Text style={styles.fieldTitle}>Email</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Email"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
-        <Text style={styles.fieldTitle}>Password</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Password"
-          secureTextEntry={true}
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
+    <View style={styles.container}>
+      <Text style={styles.title}>Chat App</Text>
+      <Text style={styles.fieldTitle}>Email</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <Text style={styles.fieldTitle}>Password</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
 
-        <TouchableOpacity style={styles.loginButton} onPress={login}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-      </View>
-    </TouchableWithoutFeedback>
+      <TouchableOpacity style={styles.loginButton} onPress={login}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <Text style={styles.errorText}>{errorText}</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  errorText: {
+    color: "red",
+    marginVertical: 20,
+  },
   container: {
     flex: 1,
     justifyContent: "center",
